9b80651ccec1d1a79aa9376ee0866efe9509591e
tomygiordev <tomasroldangiorgi@gmail.com>
2026-02-08 22:22:18 -0300

    Icono

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,7 +9,7 @@ const Index = () => {
     <>
       <Helmet>
         <title>Nictech - Reparación de Tecnología y Tienda | Entre Ríos, Argentina</title>
-        <meta name="description" content="Especialidad en reparación de smartphones, laptops y tablets. Tienda de tecnología con garantía. Compromiso con la excelencia en Lima, Perú." />
+        <meta name="description" content="Especialidad en reparación de smartphones, laptops y tablets. Tienda de tecnología con garantía. Compromiso con la excelencia en Urdinarrain, Entre Ríos." />
       </Helmet>
       <Layout>
         <HeroSection />
